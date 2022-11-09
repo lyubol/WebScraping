cf3e547fbd93a18951d3e41c4a57a1d2fb46e9fb
Lyubomir Lirkov <L_L@adatis.co.uk>
2022-11-09 07:34:20 +0200

    Added Source to Raw Noblehire.io Notebook and deleted a previous (testing) version of it.

--- Bulgarian IT Job Boards/Source to Raw Notebooks/Source to Raw (Noblehire.io).py
+++ Bulgarian IT Job Boards/Source to Raw Notebooks/Source to Raw (Noblehire.io).py
@@ -2,7 +2,9 @@
 from datetime import date
 import time
 import pandas as pd
+import json
 from flatten_json import flatten
+from pyspark.sql.functions import *
 
 # COMMAND ----------
 
@@ -20,10 +22,14 @@ current_day = "0" + str(date.today().day) if len(str(date.today().day)) == 1 els
 location_prefix = "/dbfs"
 main_path = "/mnt/adlslirkov/it-job-boards/Noblehire.io/raw/"
 posts_path = f"posts/{current_year}/{current_month}/{current_day}/"
-posts_file_name = f"noblehireio-posts-{date.today()}.csv"
 
-# Print current path
-print(f"Posts path: {posts_path}; Posts file name: {posts_file_name}")
+# print(f"Posts path: {main_path}{posts_path}; Posts file name: {posts_file_name}")
+
+# COMMAND ----------
+
+# DBTITLE 1,Create target location
+dbutils.fs.mkdirs(main_path + posts_path)
+print(f"Created: {main_path + posts_path}")
 
 # COMMAND ----------
 
@@ -31,43 +37,15 @@ print(f"Posts path: {posts_path}; Posts file name: {posts_file_name}")
 posts = scrape_Noblehire()
 
 page = 0
-flatten_posts_list = []
 while len(posts.getPosts(page)) != 0:
     page += 1
     posts_response = posts.getPosts(page)
-    for post in posts_response:
-        flatten_posts_list.append(flatten(post))
+
+    posts_response = json.dumps(posts_response)
+    
+    if len(posts_response) > 0:
+        with open(f"{location_prefix}{main_path}{posts_path}{page}.json", "w") as f:
+            f.write(posts_response)
+            f.close()
+    
     time.sleep(10)
-
-# COMMAND ----------
-
-# DBTITLE 1,Write to ADLS Raw
-# Create target location
-dbutils.fs.mkdirs(main_path + posts_path)
-print(f"Created: {main_path + posts_path}")
-
-# Since the raw data has more than 700 columns and above 300 of them are useless (e.g. icons and images ids), 
-# some of the will be droped in order to decrease the raw file size.
-df_posts = pd.DataFrame.from_dict(flatten_posts_list)
-columns_to_drop = [c for c in df_posts.columns if "icon" in c.lower() or "images" in c.lower() or "image" in c.lower()]
-df_posts = df_posts.drop(columns=columns_to_drop)
-
-# Write the Posts DataFrame to ADLS, raw location
-df_posts.to_csv(location_prefix + main_path + posts_path + posts_file_name)
-print(f"Saved at: {location_prefix + main_path + posts_path + posts_file_name}")
-
-# COMMAND ----------
-
-# df_posts["description"] = df_posts["description"].str.replace(r'<[^<>]*>', '', regex=True)
-
-# COMMAND ----------
-
-# for i in df_posts["description"]:
-#     print(i)
-#     print("------------------------------------------NEXT------------------------------------------")
-
-# COMMAND ----------
-
-# for i in df_posts["company"]:
-#     print(i)
-#     print("------------------------------------------NEXT------------------------------------------")
