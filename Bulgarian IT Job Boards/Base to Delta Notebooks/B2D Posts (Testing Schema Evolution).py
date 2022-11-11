4a788407d58b21c8d4af21b48e43b47578d929ea
Lyubomir Lirkov <L_L@adatis.co.uk>
2022-11-11 18:04:13 +0200

    Added Schema Evolution Testing Notebook

---
+++ Bulgarian IT Job Boards/Base to Delta Notebooks/B2D Posts (Testing Schema Evolution).py
@@ -0,0 +1,307 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC ### This notebook is in progress and is currently used for testing the implementation of SCD Type 2
+
+# COMMAND ----------
+
+# DBTITLE 1,Imports
+from datetime import date
+import time
+from pyspark.sql.types import *
+from pyspark.sql.functions import *
+from delta.tables import *
+
+# COMMAND ----------
+
+# DBTITLE 1,Define variables
+# Date variables 
+current_year = date.today().year
+current_month = "0" + str(date.today().month) if len(str(date.today().month)) == 1 else date.today().month
+current_day = "0" + str(date.today().day) if len(str(date.today().day)) == 1 else date.today().day
+
+# Base location variables
+location_prefix = "/dbfs"
+main_path = "/mnt/adlslirkov/it-job-boards/Noblehire.io/base/"
+temp_path = "/mnt/adlslirkov/it-job-boards/Noblehire.io/temp/"
+company_general_path = f"companyGeneral/{current_year}/{current_month}/{current_day}/"
+company_awards_path = f"companyAwards/{current_year}/{current_month}/{current_day}/"
+company_perks_path = f"companyPerks/{current_year}/{current_month}/{current_day}/"
+company_values_path = f"companyValues/{current_year}/{current_month}/{current_day}/"
+company_locations_path = f"companyLocations/{current_year}/{current_month}/{current_day}/"
+job_requirements_path = f"jobRequirements/{current_year}/{current_month}/{current_day}/"
+job_benefits_path = f"jobBenefits/{current_year}/{current_month}/{current_day}/"
+job_responsibilities_path = f"jobResponsibilities/{current_year}/{current_month}/{current_day}/"
+job_tools_path = f"jobTools/{current_year}/{current_month}/{current_day}/"
+job_activities_path = f"jobActivities/{current_year}/{current_month}/{current_day}/"
+job_hiring_process_path = f"jobHiringProcess/{current_year}/{current_month}/{current_day}/"
+posts_path = f"posts/{current_year}/{current_month}/{current_day}/"
+
+# COMMAND ----------
+
+# DBTITLE 1,Create Temp Staging Table
+# Read Base
+df_posts = spark.read.format("parquet").load(main_path + posts_path)
+
+# Create the Source Data Frame
+sourceDF = df_posts
+sourceDF.display()
+
+# COMMAND ----------
+
+sourceDF = sourceDF.withColumn("NewColumn", lit("This column is used to test schema drift behaviour in Delta Tables"))
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ### Create Database and Tables
+
+# COMMAND ----------
+
+# DBTITLE 1,Create Database
+# MAGIC %sql
+# MAGIC 
+# MAGIC -- CREATE DATABASE IF NOT EXISTS jobposts_noblehire 
+# MAGIC -- COMMENT 'This database holds job posts data coming from Noblehire.io'
+# MAGIC -- LOCATION '/mnt/adlslirkov/it-job-boards/Noblehire.io/delta/'
+
+# COMMAND ----------
+
+# DBTITLE 1,Add SCD Type 2 Columns to Delta Table
+# This command has been ran just once, when the delta table was first created.
+
+df_posts = (
+    df_posts
+    .withColumn("IsActive", lit(True))
+    .withColumn("StartDate", date_format(current_timestamp(), "yyyy-MM-dd HH:mm:ss"))
+    .withColumn("EndDate", lit(None).cast(StringType()))
+)
+
+# COMMAND ----------
+
+# DBTITLE 1,Create Delta Table
+# This command has been ran just once, when the delta table was first created.
+
+df_posts.write.format("delta").saveAsTable("jobposts_noblehire.posts")
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC 
+# MAGIC SELECT * FROM jobposts_noblehire.posts
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC 
+# MAGIC DROP TABLE jobposts_noblehire.posts
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC 
+# MAGIC DELETE FROM jobposts_noblehire.posts
+# MAGIC WHERE companyId = 1
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC 
+# MAGIC UPDATE jobposts_noblehire.posts
+# MAGIC SET description = 'Old description'
+# MAGIC WHERE companyId = 2
+
+# COMMAND ----------
+
+# DBTITLE 1,Create Delta Table Instance
+deltaPosts = DeltaTable.forPath(spark, "/mnt/adlslirkov/it-job-boards/Noblehire.io/delta/posts")
+
+targetDF = deltaPosts.toDF()
+targetDF.display()
+
+# COMMAND ----------
+
+# DBTITLE 1,Join source and target
+# Rename columns in targetDF, so that we don't need to manually alias them in the join below.
+# Since this code will be used for DataFrames with different number of columns and column names, this is the approach that we need to take.
+# targetDF = targetDF.toDF(*["target_" + column for column in targetDF.columns])
+
+targetDF = targetDF.filter(col("IsActive") == True).select(*[col for col in targetDF.columns if col not in ["IsActive", "StartDate", "EndDate"]])
+
+
+joinDF = (
+    sourceDF
+    .join(
+        targetDF, 
+        (sourceDF.id == targetDF.id),
+#         & (targetDF.IsActive == "true"),
+        "leftouter"
+    )
+    .select(
+        sourceDF["*"],
+        *[targetDF[f"{c}"].alias(f"target_{c}") for c in targetDF.columns]
+#         targetDF.companyId.alias("target_companyId"),
+#         targetDF.company_values_text_0.alias("target_company_values_text_0"),
+#         targetDF.company_values_text_1.alias("target_company_values_text_1"),
+#         targetDF.company_values_text_2.alias("target_company_values_text_2"),
+#         targetDF.company_values_text_3.alias("target_company_values_text_3"),
+#         targetDF.company_values_text_4.alias("target_company_values_text_4"),
+#         targetDF.company_values_text_5.alias("target_company_values_text_5"),
+#         targetDF.company_values_text_6.alias("target_company_values_text_6"),
+#         targetDF.company_values_text_7.alias("target_company_values_text_7"),
+#         targetDF.company_values_text_8.alias("target_company_values_text_8"),
+#         targetDF.company_values_text_9.alias("target_company_values_text_9"),
+#         targetDF.company_values_title_0.alias("target_company_values_title_0"),
+#         targetDF.company_values_title_1.alias("target_company_values_title_1"),
+#         targetDF.company_values_title_2.alias("target_company_values_title_2"),
+#         targetDF.company_values_title_3.alias("target_company_values_title_3"),
+#         targetDF.company_values_title_4.alias("target_company_values_title_4"),
+#         targetDF.company_values_title_5.alias("target_company_values_title_5"),
+#         targetDF.company_values_title_6.alias("target_company_values_title_6"),
+#         targetDF.company_values_title_7.alias("target_company_values_title_7"),
+#         targetDF.company_values_title_8.alias("target_company_values_title_8"),
+#         targetDF.company_values_title_9.alias("target_company_values_title_9"),
+#         targetDF.Source.alias("target_Source"),
+#         targetDF.IngestionDate.alias("target_IngestionDate")
+    )
+)
+
+joinDF.display()
+
+# COMMAND ----------
+
+# DBTITLE 1,Hash source and target columns and compare them
+filterDF = joinDF.filter(xxhash64(*[col for col in joinDF.columns if col.startswith("target") == False and "IngestionDate" not in col]) != xxhash64(*[col for col in joinDF.columns if col.startswith("target") == True and "IngestionDate" not in col])).withColumn("MergeKey", col("target_id"))
+
+filterDF.display()
+
+# COMMAND ----------
+
+# DBTITLE 1,Add MergeKey and set it to null where Id is not null
+dummyDF = filterDF.filter(col("target_id").isNotNull()).withColumn("MergeKey", lit(None))
+
+dummyDF.display()
+
+# COMMAND ----------
+
+# DBTITLE 1,Union DFs
+scdDF = filterDF.union(dummyDF)
+
+scdDF.display()
+
+# COMMAND ----------
+
+newColumns = [col for col in sourceDF.columns if col not in targetDF.columns]
+
+# COMMAND ----------
+
+newColumns
+
+# COMMAND ----------
+
+for column in newColumns:
+    spark.read.format("delta").load('/mnt/adlslirkov/it-job-boards/Noblehire.io/delta/posts')\
+  .withColumn(column, lit(None))\
+  .write\
+  .format("delta")\
+  .mode("overwrite")\
+  .option("overwriteSchema", "true")\
+  .save('/mnt/adlslirkov/it-job-boards/Noblehire.io/delta/posts')
+
+# COMMAND ----------
+
+display(spark.read.format("delta").load('/mnt/adlslirkov/it-job-boards/Noblehire.io/delta/posts'))
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC 
+# MAGIC ALTER TABLE jobposts_noblehire.posts
+# MAGIC ADD COLUMNS (NewColumn STRING)
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC 
+# MAGIC SELECT * FROM jobposts_noblehire.posts
+
+# COMMAND ----------
+
+# DBTITLE 1,Merge
+(deltaPosts.alias("target")
+ .merge(
+     scdDF.alias("source"),
+     "target.id = source.MergeKey"
+ )
+ .whenMatchedUpdate(set = 
+    {
+        "Source": "source.Source",
+        "IsActive": "'False'", 
+        "EndDate": "date_format(current_timestamp(), 'yyyy-MM-dd HH:mm:ss')"
+    }
+ )
+ .insertAll()
+ .whenNotMatchedInsert(values =
+#         columns_dict
+     {
+        "id": "source.id",
+        "companyId": "source.companyId",
+        "Source": "source.Source",
+        "IngestionDate": "source.IngestionDate",
+        "postedAt_Timestamp": "source.postedAt_Timestamp",
+        "businessTravelComment": "source.businessTravelComment",
+        "businessTraveling": "source.businessTraveling",
+        "customerFacing": "source.customerFacing",
+        "description": "source.description",
+        "fullyRemote": "source.fullyRemote",
+        "homeOfficeDays": "source.homeOfficeDays",
+        "homeOfficePer": "source.homeOfficePer",
+        "jobType": "source.jobType",
+        "jobTypeComment": "source.jobTypeComment",
+        "mainDatabase": "source.mainDatabase",
+        "offeringStock": "source.offeringStock",
+        "postedAt": "source.postedAt",
+        "primaryLanguage": "source.primaryLanguage",
+        "primaryPlatform": "source.primaryPlatform",
+        "productDescription": "source.productDescription",
+        "public": "source.public",
+        "role": "source.role",
+        "salaryCurrency": "source.salaryCurrency",
+        "salaryMax": "source.salaryMax",
+        "salaryMin": "source.salaryMin",
+        "salaryPeriod": "source.salaryPeriod",
+        "secondaryLanguage": "source.secondaryLanguage",
+        "secondaryPlatform": "source.secondaryPlatform",
+        "seniority": "source.seniority",
+        "slug": "source.slug",
+        "teamLead": "source.teamLead",
+        "teamLeadName": "source.teamLeadName",
+        "teamLeadRole": "source.teamLeadRole",
+        "teamSizeMax": "source.teamSizeMax",
+        "teamSizeMin": "source.teamSizeMin",
+        "title": "source.title",
+        "NewColumn": "source.NewColumn",
+        "IsActive": "'True'",
+        "StartDate": "date_format(current_timestamp(), 'yyyy-MM-dd HH:mm:ss')"
+#         "EndDate": """to_date('9999-12-31 00:00:00.0000', 'MM-dd-yyyy HH:mm:ss')"""
+     }
+ )
+ .execute()
+)
+
+# COMMAND ----------
+
+columns_dict = {col: "source." + col for col in df_posts.columns}
+columns_dict["IsActive"] = "'True'"
+columns_dict["StartDate"] = "date_format(current_timestamp(), 'yyyy-MM-dd HH:mm:ss')"
+# columns_dict["EndDate"] = """to_date('9999-12-31 00:00:00.0000', 'MM-dd-yyyy HH:mm:ss')"""
+
+columns_dict
+
+# COMMAND ----------
+
+# DBTITLE 1,Check Delta Table History
+deltaPosts.history().display()
+
+# COMMAND ----------
+
+
